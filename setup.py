bae0ff1fce8fecfab15bb9c0f1098196d32578a8
BubblePlayz <bubblelind@gmail.com>
2025-05-05 18:00:09 -0500

    updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="anondrop",
-    version="0.1.0",
+    version="0.1.1",
     author="BubblePlayz",
     author_email="mylaptop4768@gmail.com",
     description="A package for uploading and deleting files using AnonDrop API",
